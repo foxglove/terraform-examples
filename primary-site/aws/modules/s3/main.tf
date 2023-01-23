ff697eca0a794779a63de914ecbfd1c5b78f786a
Richard Dancsi <richard@foxglove.dev>
2023-01-23 05:42:36 -0500

    enhancement(AWS): bucket lifecycle rule to delete incomplete multipart upload parts

--- primary-site/aws/modules/s3/main.tf
+++ primary-site/aws/modules/s3/main.tf
@@ -6,3 +6,16 @@ resource "aws_s3_bucket_acl" "bucket_acl" {
   bucket = aws_s3_bucket.bucket.id
   acl    = "private"
 }
+
+resource "aws_s3_bucket_lifecycle_configuration" "bucket_lifecycle" {
+  bucket = aws_s3_bucket.bucket.id
+
+  rule {
+    id     = "${var.bucket_name}-delete-incomplete-uploads-rule"
+    status = "Enabled"
+
+    abort_incomplete_multipart_upload {
+      days_after_initiation = var.abort_incomplete_multipart_upload_days
+    }
+  }
+}
--- primary-site/aws/modules/s3/variables.tf
+++ primary-site/aws/modules/s3/variables.tf
@@ -2,3 +2,8 @@ variable "bucket_name" {
   type        = string
   description = "Name of the S3 bucket"
 }
+
+variable "abort_incomplete_multipart_upload_days" {
+  type        = number
+  description = "Number of days a multipart upload needs to be completed within"
+}
