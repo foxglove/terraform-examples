23eac98f95006cd108a14ade452b7ca5fced8659
Richard Dancsi <richard@foxglove.dev>
2023-09-27 09:30:39 -0400

    feature(AWS): configures an SQS queue to be used as DLQ with the SNS subscription

--- primary-site/aws/modules/s3/main.tf
+++ primary-site/aws/modules/s3/main.tf
@@ -2,11 +2,6 @@ resource "aws_s3_bucket" "bucket" {
   bucket = var.bucket_name
 }
 
-resource "aws_s3_bucket_acl" "bucket_acl" {
-  bucket = aws_s3_bucket.bucket.id
-  acl    = "private"
-}
-
 resource "aws_s3_bucket_lifecycle_configuration" "bucket_lifecycle" {
   bucket = aws_s3_bucket.bucket.id
 
