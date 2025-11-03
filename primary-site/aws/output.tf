91a6da15c6be9419b4d967c2837071362d5ee686
Bennett Hardwick <bennett@foxglove.dev>
2025-11-03 11:50:10 +1100

    Update service account to be query service

--- primary-site/aws/output.tf
+++ primary-site/aws/output.tf
@@ -7,3 +7,18 @@ output "eks_vpc_arn" {
   value       = module.vpc.vpc_arn
   description = "VPC ID of the EKS cluster"
 }
+
+output "iam_inbox_listener_role_arn" {
+  value       = module.iam.iam_inbox_listener_role_arn
+  description = "ARN for the role to be added to inbox listener pods"
+}
+
+output "iam_query_service_role_arn" {
+  value       = module.iam.iam_query_service_role_arn
+  description = "ARN for the role to be added to query service pods"
+}
+
+output "iam_garbage_collector_role_arn" {
+  value       = module.iam.iam_garbage_collector_role_arn
+  description = "ARN for the role to be added to garbage collector pods"
+}
